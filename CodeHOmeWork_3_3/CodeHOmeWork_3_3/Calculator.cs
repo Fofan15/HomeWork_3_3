43b19c6a839df8c37cbda1f0f4e230c2b0ec926d
Fofan15 <daniil.grishin16@gmail.com>
2023-03-15 18:59:21 +0200

    Add a code

---
+++ CodeHOmeWork_3_3/CodeHOmeWork_3_3/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeHOmeWork_3_3
+{
+    public delegate int SumDelegate(int x, int y);
+    public class Calculator
+    {
+        public event SumDelegate SumEvent;
+
+        public void Calculate() 
+        {
+            SumDelegateWrapper(SumEvent);
+        }
+
+        private void SumDelegateWrapper(SumDelegate sumDelegate) 
+        {
+            int result = 0;
+            try
+            {
+                foreach (SumDelegate sum in sumDelegate.GetInvocationList())
+                {
+                    result += sum(3, 1);
+                }
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
